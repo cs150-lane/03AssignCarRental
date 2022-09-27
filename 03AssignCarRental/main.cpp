25a8c676ec3bf289282f2a12363e6f007fa496f2
Chris Lane <lanec@pacificu.edu>
2022-09-26 17:18:34 -0700

    Add project files.

---
+++ 03AssignCarRental/main.cpp
@@ -0,0 +1,18 @@
+//**********************************************************************
+// File:					main.cpp
+// Author:				
+// Date:					
+// Class:					
+// Assignment:		
+// Purpose:				
+// Hours Worked:	
+//**********************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	return EXIT_SUCCESS;
+}
